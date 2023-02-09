909284fae47a86420b3c323e320e8110805ffb14
marcosvnl <maarcosvnl@gmail.com>
2023-02-08 22:23:46 -0300

    First

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <>
+        <h1>Em breve página de dev marcosvnl</h1>
+        <strong>.NET | C# | T-SQL | React | Docker</strong>
+    </>
+  )
+}
+
+export default LandingPage
