92f0c87769af5918afb0312709a3cda7b136caa3
marcosvnl <maarcosvnl@gmail.com>
2023-02-10 18:28:17 -0300

    css init

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import MyPhoto from '../assets/myPhoto.png'
 
 const LandingPage = () => {
   return (
-    <>
+    <div>
+        <img className='my-photo' src={ MyPhoto } alt="Eu" />
         <h1>Em breve página de dev marcosvnl</h1>
         <strong>.NET | C# | T-SQL | React | Docker</strong>
-    </>
+    </div>
   )
 }
 
