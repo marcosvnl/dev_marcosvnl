3dfe7443a90c6044257502b9ade4a033b5072941
marcosvnl <maarcosvnl@gmail.com>
2023-02-22 22:45:03 -0300

    Ajustes na LandingPage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import './LandingPage.css'
 import MyPhoto from '../assets/myPhoto.png'
 
 const LandingPage = () => {
   return (
-    <div>
-        <img className='my-photo' src={ MyPhoto } alt="Eu" />
-        <h1>Em breve página de dev marcosvnl</h1>
-        <strong>.NET | C# | T-SQL | React | Docker</strong>
+    <div className='landing-page'>
+        <img src={ MyPhoto } alt="Eu" />
+        <div className="context">
+          <h1>Em breve página de aprensentação</h1>
+          <strong>.NET | C# | REST API | T-SQL | React | Docker</strong>
+        </div>
     </div>
   )
 }
